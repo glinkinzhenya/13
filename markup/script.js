2692554c8adab86a8ba6c4d271f541d697d0896e
glinkinzhenya <glinkinzhenya@gmail.com>
2022-11-25 11:56:07 +0200

    3

--- markup/script.js
+++ markup/script.js
@@ -7,26 +7,20 @@ image = "";
 br = 9;
 
 for (j = 0; j < 13; j++) {
-
     if (j === br) {
         br++;
         image = `<img class="person" src="images/${icon[br - 10]}.svg" alt="${icon[br - 10]}"></img>`;
     }
-
     for (i = 0; i < 4; i++) {
-
         if (j === 12) {
             image = `<img class="person" src="images/${suit[i]}.svg" alt="${suit[i]}"></img>`;
         }
-
         card.push(`<div class="card">
 		<div class="card__info">${number[j]}<img src="images/${suit[i]}.svg" alt="${suit[i]}"></div>
         ${image}
 		<div class="card__info">${number[j]}<img src="images/${suit[i]}.svg" alt="${suit[i]}"></div>
 		</div>`)
-
     }
 }
-
 document.write(`<div class="wrapper">${card.join("")}<div>`);
 
